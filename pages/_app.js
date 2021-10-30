9e3f161544cea8f10be07df64d84028b500ecc3b
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-10-30 18:47:57 -0400

    Added fontawesome and animated fontawesome

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,17 @@ import Layout from "../components/ui/Layout/Layout";
 import LayoutMaximilian from "../components/ui/LayoutMaximilian/LayoutMaximilian";
 import generateHeadInfo from "../tools/generateHeadInfo";
 
+// Fontawesome:
+import {config, library} from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import {fab} from "@fortawesome/free-brands-svg-icons";
+import '@fortawesome/fontawesome-free/css/all.css';
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+library.add(fab);
+
+// FontAwesome Animation:
+import 'font-awesome-animation/font-awesome-animation.scss';
+
 function MyApp({Component, pageProps}) {
     const headInfo = generateHeadInfo(Component.name, pageProps);
 
