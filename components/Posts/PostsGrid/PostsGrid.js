10ac0f8a3d430d3f91b2455cd36073db69a21eaf
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-10-30 14:59:48 -0400

    Improved the components/Posts/PostsGrid/PostsGrid.js

--- components/Posts/PostsGrid/PostsGrid.js
+++ components/Posts/PostsGrid/PostsGrid.js
@@ -2,9 +2,15 @@ import styles from './PostsGrid.module.css';
 import {Fragment} from "react";
 import PostGridItem from "../PostGridItem/PostGridItem";
 
-const PostsGrid = () => {
+const PostsGrid = (props) => {
+    const {posts} = props;
+    const postsGridItems = (posts || []).map(post => <PostGridItem key={post.id} post={post}/>);
+
     return (
         <Fragment>
+            <ul className={styles.grid}>
+                {postsGridItems}
+            </ul>
         </Fragment>
     );
 
