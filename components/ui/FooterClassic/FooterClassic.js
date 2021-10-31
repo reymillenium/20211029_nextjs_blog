0ad8835138ddeda6116bd74a15df48acef30e77b
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-10-31 10:58:03 -0400

    Improved the footer by adding new links and flags

--- components/ui/FooterClassic/FooterClassic.js
+++ components/ui/FooterClassic/FooterClassic.js
@@ -1,7 +1,12 @@
 import styles from './FooterClassic.module.css';
+import Image from "next/image";
 
 
 const FooterClassic = () => {
+    const websiteUrl = 'https://reiniergarcia.dev';
+    const linkedinUrl = 'https://www.linkedin.com/in/reymillenium';
+    const gitHubUrl = 'https://github.com/reymillenium';
+    const stackOverflowUrl = 'https://stackoverflow.com/users/9616949/reinier-garcia?tab=profile';
 
     const addAnimationClassHandler = (event) => {
         // setTimeout(function () {
@@ -38,29 +43,34 @@ const FooterClassic = () => {
                     </div>
                     <div className={styles.Column}>
                         <p className={styles.Title}>Contact Us</p>
-                        <a className={styles.Link} href="#">United States</a>
-                        <a className={styles.Link} href="#">United Kingdom</a>
-                        <a className={styles.Link} href="#">Australia</a>
-                        <a className={styles.Link} href="#">Support</a>
+                        {/*<a className={styles.Link} href="#">United States</a>*/}
+                        {/*<a className={styles.Link} href="#">United Kingdom</a>*/}
+                        {/*<a className={styles.Link} href="#">Australia</a>*/}
+                        {/*<a className={styles.Link} href="#">Support</a>*/}
+                        <a className={styles.Link} href="#"><Image src={'/images/site/flags/us.png'} width={16} height={11}/>&nbsp;&nbsp;&nbsp;United States</a>
+                        <a className={styles.Link} href="#"><Image src={'/images/site/flags/gb.png'} width={16} height={11}/>&nbsp;&nbsp;&nbsp;United Kingdom</a>
+                        <a className={styles.Link} href="#"><Image src={'/images/site/flags/au.png'} width={16} height={11}/>&nbsp;&nbsp;&nbsp;Australia</a>
+                        <a className={styles.Link} href="#"><Image src={'/images/site/flags/us.png'} width={16} height={11}/>&nbsp;&nbsp;&nbsp;Support</a>
                     </div>
 
                     <div className={styles.Column}>
                         <p className={styles.Title}>Social</p>
 
-                        <a className={styles.Link + ' faa-parent animated-hover'} href="#" onMouseEnter={removeAnimationClassHandler} onMouseLeave={addAnimationClassHandler}>
-                            <i className={styles.Icon + " fab fa-facebook-f faa-ring faa-slow"}/>Facebook
+                        <a className={styles.Link + ' faa-parent animated-hover'} href={websiteUrl} target={'_website'} onMouseEnter={removeAnimationClassHandler} onMouseLeave={addAnimationClassHandler}>
+                            <i className={styles.Icon + " fas fa-globe faa-pulse faa-fast"}/>Website
                         </a>
 
-                        <a className={styles.Link + ' faa-parent animated-hover'} href="#" onMouseEnter={removeAnimationClassHandler} onMouseLeave={addAnimationClassHandler}>
-                            <i className={styles.Icon + " fab fa-instagram faa-pulse faa-fast"}/>Instagram
+                        <a className={styles.Link + ' faa-parent animated-hover'} href={linkedinUrl} target={'_linkedin'} onMouseEnter={removeAnimationClassHandler} onMouseLeave={addAnimationClassHandler}>
+                            <i className={styles.Icon + " fab fa-linkedin faa-ring faa-slow"}/>Linkedin
+                            {/*<i className={styles.Icon + " fab fa-linkedin-in faa-ring faa-slow"}/>Linkedin*/}
                         </a>
 
-                        <a className={styles.Link + ' faa-parent animated-hover'} href="#" onMouseEnter={removeAnimationClassHandler} onMouseLeave={addAnimationClassHandler}>
-                            <i className={styles.Icon + " fab fa-youtube faa-tada faa-fast"}/>Youtube
+                        <a className={styles.Link + ' faa-parent animated-hover'} href={gitHubUrl} target={'_github'} onMouseEnter={removeAnimationClassHandler} onMouseLeave={addAnimationClassHandler}>
+                            <i className={styles.Icon + " fab fa-github faa-tada faa-fast"}/>GitHub
                         </a>
 
-                        <a className={styles.Link + ' faa-parent animated-hover'} href="#" onMouseEnter={removeAnimationClassHandler} onMouseLeave={addAnimationClassHandler}>
-                            <i className={styles.Icon + " fab fa-twitter faa-wrench faa-fast"}/>Twitter
+                        <a className={styles.Link + ' faa-parent animated-hover'} href={stackOverflowUrl} target={'_stackoverflow'} onMouseEnter={removeAnimationClassHandler} onMouseLeave={addAnimationClassHandler}>
+                            <i className={styles.Icon + " fab fa-stack-overflow faa-wrench faa-fast"}/>Stack Overflow
                         </a>
                     </div>
                 </div>
