ae6db561cb7ce109d56a523a491355d2b8eec426
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-11-01 17:26:44 -0400

    Added another md file

--- pages/index.js
+++ pages/index.js
@@ -3,28 +3,6 @@ import Hero from "../components/ui/Hero/Hero";
 import FeaturedPosts from "../components/Posts/FeaturedPosts/FeaturedPosts";
 import {getAllPosts} from "../lib/markdownUtils";
 
-// export const dummyPosts = [
-//     {
-//         id: 'p1',
-//         title: 'Post 1',
-//         summary: 'Post 1 summary',
-//         image: 'getting-started-nextjs.png',
-//         date: '2020-04-10',
-//         content: '# This is a first post',
-//         isFeatured: true,
-//     },
-//     {
-//         id: 'p2',
-//         title: 'Post 2',
-//         summary: 'Post 2 summary',
-//         image: 'nextjs-file-based-routing.png',
-//         date: '2021-08-11',
-//         content: '# This is a second post',
-//         isFeatured: true,
-//     },
-// ];
-
-
 const HomePage = (props) => {
     const {posts} = props;
 
