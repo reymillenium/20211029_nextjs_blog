b11f9ab842a088e4fbdc5e1f9c8e37e768dfb8a8
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-11-02 13:01:37 -0400

    Coded the basic framing of the components/Contact/ContactForm/ContactForm.js

--- components/Contact/ContactForm/ContactForm.js
+++ components/Contact/ContactForm/ContactForm.js
@@ -1,7 +1,38 @@
 import styles from './ContactForm.module.css';
+import {Fragment} from "react";
 
 const ContactForm = () => {
 
+    return (
+        <Fragment>
+            <section className={styles.contact}>
+                <h1>How can I help you?</h1>
+
+                <form className={styles.form}>
+                    <div className={styles.controls}>
+                        <div className={styles.control}>
+                            <label htmlFor="email">Your Email</label>
+                            <input type="email" id={'email'} name={'email'}/>
+                        </div>
+
+                        <div className={styles.control}>
+                            <label htmlFor="name">Your Name</label>
+                            <input type="text" id={'name'} name={'name'}/>
+                        </div>
+                    </div>
+
+                    <div className={styles.control}>
+                        <label htmlFor="message">Message:</label>
+                        <textarea name="message" id="message" rows="5"></textarea>
+                    </div>
+
+                    <div className={styles.actions}>
+                        <button>Send Message</button>
+                    </div>
+                </form>
+            </section>
+        </Fragment>
+    );
 };
 
 export default ContactForm;
--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,10 @@
 import {Fragment} from "react";
+import ContactForm from "../components/Contact/ContactForm/ContactForm";
 
 const ContactPage = () => {
     return (
         <Fragment>
+            <ContactForm/>
         </Fragment>
     );
 };
