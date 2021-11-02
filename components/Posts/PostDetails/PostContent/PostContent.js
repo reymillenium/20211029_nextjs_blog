789238654eb82faa095e2bcc3bf47107dd812b31
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-11-02 11:10:20 -0400

    Especified the components passing renderers to the ReactMarkdown component at components/Posts/PostDetails/PostContent/PostContent.js

--- components/Posts/PostDetails/PostContent/PostContent.js
+++ components/Posts/PostDetails/PostContent/PostContent.js
@@ -3,16 +3,51 @@ import styles from './PostContent.module.css';
 import PostHeader from "../PostHeader/PostHeader";
 import Markdown from "markdown-to-jsx";
 import ReactMarkdown from "react-markdown";
-import remarkGfm from 'remark-gfm'
+import remarkGfm from 'remark-gfm';
+import Image from "next/image";
 
 const PostContent = (props) => {
     const {post} = props;
     // const {id: postId, title, image} = props.post;
     // const imagePath = `/images/posts/${postId}/${image}`;
 
+
+    const customRenderers = {
+        // img(image) {
+        //   console.log('image', image.src)
+        //   return (
+        //     < Image
+        //       src={`${image.src}`}
+        //       alt={image.alt}
+        //       width={600}
+        //       height={300}
+        //     />
+        //   );
+        // },
+
+        p(paragraph) {
+            const { node } = paragraph;
+            if (node.children[0].tagName === 'img') {
+                const image = node.children[0];
+                return (
+                    <div className={styles.image}>
+                        < Image
+                            // src={`/images/posts/${post.id}/${image.properties.src}`}
+                            src={`${image.properties.src}`}
+                            alt={image.alt}
+                            width={600}
+                            height={300}
+                        />
+                    </div>
+                );
+            }
+            return <p>{paragraph.children}</p>;
+        }
+    }
+
     return (
         <Fragment>
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>
+            <ReactMarkdown remarkPlugins={[remarkGfm]} components={customRenderers}>
                 {post && post.content && post.content}
             </ReactMarkdown>
         </Fragment>
